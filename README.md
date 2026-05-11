20ff9139ac6e1f6cb6d2bd3672d0529fe00fb652
Pedro Lucas <plfm3k@gmail.com>
2026-05-11 17:04:20 -0400

    chore: commit inicial

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sistema de Gestão Escolar
+
+Sistema escolar para auxiliar a gestão de alunos.
+
+---
+
+## Stack:
+- Java 21
+- Spring Boot
+- PostgreSQL
+- JWT Authentication
+- Spring Security
+- Docker
---
+++ src/test/java/dev/pdrolcs/sistemagestaoescolar/SistemaGestaoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pdrolcs.sistemagestaoescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
