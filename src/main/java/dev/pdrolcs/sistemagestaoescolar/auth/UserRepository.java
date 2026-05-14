96af2ee65c871a91da2eb176ce3e786a655c637f
Pedro Lucas <plfm3k@gmail.com>
2026-05-13 21:58:40 -0400

    feat: adiciona implementação do repositorio User

---
+++ src/main/java/dev/pdrolcs/sistemagestaoescolar/auth/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.pdrolcs.sistemagestaoescolar.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<UserDetails> findUserByEmail(String username);
+
+}
