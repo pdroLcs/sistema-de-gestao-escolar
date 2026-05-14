c43ae126ad041174937f51d862b62feb27dc5235
Pedro Lucas <plfm3k@gmail.com>
2026-05-14 09:34:40 -0400

    refactor: extrai lógica de autenticação para AuthService

--- src/main/java/dev/pdrolcs/sistemagestaoescolar/auth/AuthController.java
+++ src/main/java/dev/pdrolcs/sistemagestaoescolar/auth/AuthController.java
@@ -2,11 +2,8 @@ package dev.pdrolcs.sistemagestaoescolar.auth;
 
 import dev.pdrolcs.sistemagestaoescolar.auth.dto.LoginRequest;
 import dev.pdrolcs.sistemagestaoescolar.auth.dto.LoginResponse;
-import dev.pdrolcs.sistemagestaoescolar.config.TokenConfig;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,20 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final TokenConfig tokenConfig;
+    private final AuthService authService;
 
-    public AuthController(AuthenticationManager authenticationManager, TokenConfig tokenConfig) {
-        this.authenticationManager = authenticationManager;
-        this.tokenConfig = tokenConfig;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
-        var userAndPassword = new UsernamePasswordAuthenticationToken(request.email(), request.password());
-        var authentication = authenticationManager.authenticate(userAndPassword);
-        var user = (User) authentication.getPrincipal();
-        String token = tokenConfig.generateToken(user);
-        return ResponseEntity.ok(new LoginResponse(token));
+        return ResponseEntity.ok(authService.login(request));
     }
 }
